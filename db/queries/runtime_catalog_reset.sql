93ff26cfd9dcb3d792c15dc04aa6fa131db52a33
Romonaga <robertwinslow@comcast.net>
2026-05-21 15:55:12 -0400

    Add runtime catalog reset query

---
+++ db/queries/runtime_catalog_reset.sql
@@ -0,0 +1,14 @@
+TRUNCATE TABLE
+    response_cache_chat_turns,
+    response_cache_sources,
+    response_cache_entries,
+    query_log_sources,
+    query_logs,
+    ingest_run_documents,
+    ingest_runs,
+    chunk_quality_flags,
+    document_images,
+    document_chunks,
+    document_pages,
+    documents
+RESTART IDENTITY;
